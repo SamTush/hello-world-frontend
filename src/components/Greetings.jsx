4566b88d7129c771ecf560e217045ba551936a47
Samson Tush <samo4421@icloud.com>
2023-08-04 22:52:26 +0300

    Add store, actions and reducers

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/actions';
+
+function Greetings() {
+
+  const dispatch = useDispatch();
+  const { greeting, loading, error } = useSelector((state) => state.greeting);
+  
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  return (
+    <div className="flex justify-center font-bold text-5xl mt-9">
+      <h1>Random Greeting: {loading ? 'Loading...' : greeting}</h1>
+      {error && <p>Error: {error}</p>}
+    </div>
+  )
+}
+
+export default Greetings
---
+++ src/redux/reducers.js
@@ -0,0 +1,36 @@
+import { FETCH_RANDOM_GREETING_REQUEST, FETCH_RANDOM_GREETING_SUCCESS, FETCH_RANDOM_GREETING_FAILURE } from './actions';
+
+const initialState = {
+  greeting: '',
+  loading: false,
+  error: null
+};
+
+const greetingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_RANDOM_GREETING_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case FETCH_RANDOM_GREETING_SUCCESS:
+      return {
+        ...state,
+        greeting: action.payload,
+        loading: false,
+        error: null
+      };
+    case FETCH_RANDOM_GREETING_FAILURE:
+      return {
+        ...state,
+        greeting: '',
+        loading: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+export default greetingReducer;
